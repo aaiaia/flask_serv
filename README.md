c000c7f023d77ee3b73d381327ddfc3d62b58643
PleYa <aaiaia.gg@gmail.com>
2022-10-05 19:26:14 +0900

    Example swagger (#1)
    * upload swagger example file
    
    * updated README.md to use flask-restx
    
    * added no namespace case
    
    * added arguments of api
    
    * added query args parsing examples
    
    * relocated statements, api arguments are just used in target api
    
    * renamed variable, df_get_parser -> _parser
    
    * fixed response, echo back arguments
    
    * added model of put
    
    * added example codes about getting value of keys
    
    * fixed response of put method, echo back input data
    
    * renamed variable, api -> api_default
    
    * renamed variable, api_ns -> api_sub_0
    
    * renamed variable, _fields -> _model
    
    * added default methods post and delete
    
    * added args of get in sub api
    
    * fixed sub function get method response echo values
    
    * added args to put method and params(args and json) to post method
    
    * added download api(namespace)
    
    * replaced statements
    
    * fixed typo
    
    * added class to download file
    
    * renamed api_dl_file -> api_download
    
    * added api for downloading file
    
    * minar added comments
    
    * moved example_swagger.py -> swagger/example_swagger.py
    
    * uploaded sample text file for swagger api
    
    * renamed example_swagger.py -> swagger_apisvr.py

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # flask_serv
+
+## swagger
+Required python package
+```bash
+$ pip install flask-restx
+$ pip install Werkzeug==1.1.2
+```
+confirm python package
+```bash
+$ pip3 list | grep flask-restx
+flask-restx                    0.5.1
+$ pip3 list |grep Werkzeug
+Werkzeug                       2.1.2
+```
---
+++ swagger/swagger_apisvr.py
@@ -0,0 +1,170 @@
+from pathlib import Path
+
+from flask import Flask, request, jsonify, send_file, abort
+from flask_restx import Api, Resource, reqparse, Namespace, fields
+
+# -----------------------------------------------------
+# flask and api setting
+# -----------------------------------------------------
+app = Flask(__name__)
+api_default = Api(app, version='1.0', title='test api list',description='테스트 REST API 문서',)
+api_sub_0 = api_default.namespace('sub_0', description='hello world API')
+api_download = api_default.namespace('download', description='download files')
+
+# -----------------------------------------------------
+# default api list
+# -----------------------------------------------------
+@api_default.route('/hello')
+class getHelloWorld(Resource):
+    _parser = reqparse.RequestParser()
+    _parser.add_argument('value', type=int, default=0, help='값 입력')
+    _parser.add_argument('text', type=str, default='', help='문자열 입력')
+
+    @api_default.expect(_parser)
+    def get(self):  # get method can't have body, means that can't use json to argument
+        _query_params = request.args
+        print('_query_params: ' + str(_query_params))
+
+        _value = _query_params.get('value', 0)
+        _text = _query_params.get('text', '')
+        print('_value: ' + str(_value))
+        print('_text: ' + str(_text))
+
+        _response = {
+            'hello': 'world',
+            'value': str(_value),
+            'text': str(_text)
+        }
+        return _response
+
+    _parser = reqparse.RequestParser()
+    _parser.add_argument('value', type=int, default=0, help='값 입력')
+    _parser.add_argument('text', type=str, default='', help='문자열 입력')
+
+    _model = api_default.model('put', {
+            'key1': fields.String(description='value of key1', required=True, example='value1'),
+            'key2': fields.String(description='value of key2', required=True, example='value2'),
+            })
+
+    @api_default.expect(_parser, _model)
+    def put(self):
+        _query_params = request.args
+        print('_query_params: ' + str(_query_params))
+
+        _value = _query_params.get('value', 0)
+        _text = _query_params.get('text', '')
+        print('_value: ' + str(_value))
+        print('_text: ' + str(_text))
+
+        _data = request.json
+        print('_data: ' + str(_data))
+
+        _key1_value = _data.get('key1', '')
+        _key2_value = _data.get('key2', '')
+        print('_key1_value: ' + str(_key1_value))
+        print('_key2_value: ' + str(_key2_value))
+
+        _response = {
+            'hello': 'world',
+            'value': str(_value),
+            'text': str(_text),
+            'data': {
+                'key1': str(_key1_value),
+                'key2': str(_key2_value)
+            }
+        }
+        return _response
+
+    _parser = reqparse.RequestParser()
+    _parser.add_argument('value', type=int, default=0, help='값 입력')
+    _parser.add_argument('text', type=str, default='', help='문자열 입력')
+
+    _model = api_default.model('put', {
+            'key1': fields.String(description='value of key1', required=True, example='value1'),
+            'key2': fields.String(description='value of key2', required=True, example='value2'),
+            })
+
+    @api_default.expect(_parser, _model)
+    def post(self):
+        _query_params = request.args
+        print('_query_params: ' + str(_query_params))
+
+        _value = _query_params.get('value', 0)
+        _text = _query_params.get('text', '')
+        print('_value: ' + str(_value))
+        print('_text: ' + str(_text))
+
+        _data = request.json
+        print('_data: ' + str(_data))
+
+        _key1_value = _data.get('key1', '')
+        _key2_value = _data.get('key2', '')
+        print('_key1_value: ' + str(_key1_value))
+        print('_key2_value: ' + str(_key2_value))
+
+        _response = {
+            'hello': 'world',
+            'value': str(_value),
+            'text': str(_text),
+            'data': {
+                'key1': str(_key1_value),
+                'key2': str(_key2_value)
+            }
+        }
+        return _response
+
+    def delete(self):
+        return {'hello': 'world'}
+
+# -----------------------------------------------------
+# sub_0 api list
+# -----------------------------------------------------
+@api_sub_0.route('/hello')
+class aiagent_getHelloWorld(Resource):
+    _parser = reqparse.RequestParser()
+    _parser.add_argument('value', type=int, default=0, help='값 입력')
+    _parser.add_argument('text', type=str, default='', help='문자열 입력')
+
+    @api_default.expect(_parser)
+    def get(self):
+        _query_params = request.args
+        print('_query_params: ' + str(_query_params))
+
+        _value = _query_params.get('value', 0)
+        _text = _query_params.get('text', '')
+        print('_value: ' + str(_value))
+        print('_text: ' + str(_text))
+
+        _response = {
+            'hello': 'world',
+            'value': str(_value),
+            'text': str(_text),
+            'data': {
+                'dummykey0': 'value0',
+                'dummykey1': 'value1'
+            }
+        }
+        return _response
+
+# -----------------------------------------------------
+# download api list
+# -----------------------------------------------------
+@api_download.route('/file/<name>')
+@api_download.doc(params={'name': 'insert file name with extention, if needs'})
+class download_file(Resource):
+    @api_download.response(200, 'file founds')
+    @api_download.response(400, 'file not founds')
+    def get(self, name):
+        _path = './'
+        _fileName = name
+        _filePath = _path + _fileName
+        if Path(_filePath).exists():
+            _response = send_file(_filePath, as_attachment=True)
+        else:
+            _response = abort(status=400, description='file not founds')
+        return _response
+
+# -----------------------------------------------------
+# -----------------------------------------------------
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)  # , debug=True)
