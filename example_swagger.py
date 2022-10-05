8bf2f07e550f3cc0ebba2949450b559c5d0ef372
PleYa <aaiaia.gg@gmail.com>
2022-10-05 18:35:26 +0900

    added args to put method and params(args and json) to post method

--- example_swagger.py
+++ example_swagger.py
@@ -18,7 +18,7 @@ class getHelloWorld(Resource):
     _parser.add_argument('text', type=str, default='', help='문자열 입력')
 
     @api_default.expect(_parser)
-    def get(self):
+    def get(self):  # get method can't have body, means that can't use json to argument
         _query_params = request.args
         print('_query_params: ' + str(_query_params))
 
@@ -34,13 +34,25 @@ class getHelloWorld(Resource):
         }
         return _response
 
+    _parser = reqparse.RequestParser()
+    _parser.add_argument('value', type=int, default=0, help='값 입력')
+    _parser.add_argument('text', type=str, default='', help='문자열 입력')
+
     _model = api_default.model('put', {
-            'key1': fields.String(description='value of key1', required=True, example='String type value'),
-            'key2': fields.String(description='value of key2', required=True, example='String type value'),
+            'key1': fields.String(description='value of key1', required=True, example='value1'),
+            'key2': fields.String(description='value of key2', required=True, example='value2'),
             })
 
-    @api_default.expect(_model)
+    @api_default.expect(_parser, _model)
     def put(self):
+        _query_params = request.args
+        print('_query_params: ' + str(_query_params))
+
+        _value = _query_params.get('value', 0)
+        _text = _query_params.get('text', '')
+        print('_value: ' + str(_value))
+        print('_text: ' + str(_text))
+
         _data = request.json
         print('_data: ' + str(_data))
 
@@ -51,13 +63,52 @@ class getHelloWorld(Resource):
 
         _response = {
             'hello': 'world',
-            'key1': str(_key1_value),
-            'key2': str(_key2_value)
+            'value': str(_value),
+            'text': str(_text),
+            'data': {
+                'key1': str(_key1_value),
+                'key2': str(_key2_value)
+            }
         }
         return _response
 
+    _parser = reqparse.RequestParser()
+    _parser.add_argument('value', type=int, default=0, help='값 입력')
+    _parser.add_argument('text', type=str, default='', help='문자열 입력')
+
+    _model = api_default.model('put', {
+            'key1': fields.String(description='value of key1', required=True, example='value1'),
+            'key2': fields.String(description='value of key2', required=True, example='value2'),
+            })
+
+    @api_default.expect(_parser, _model)
     def post(self):
-        return {'hello': 'world'}
+        _query_params = request.args
+        print('_query_params: ' + str(_query_params))
+
+        _value = _query_params.get('value', 0)
+        _text = _query_params.get('text', '')
+        print('_value: ' + str(_value))
+        print('_text: ' + str(_text))
+
+        _data = request.json
+        print('_data: ' + str(_data))
+
+        _key1_value = _data.get('key1', '')
+        _key2_value = _data.get('key2', '')
+        print('_key1_value: ' + str(_key1_value))
+        print('_key2_value: ' + str(_key2_value))
+
+        _response = {
+            'hello': 'world',
+            'value': str(_value),
+            'text': str(_text),
+            'data': {
+                'key1': str(_key1_value),
+                'key2': str(_key2_value)
+            }
+        }
+        return _response
 
     def delete(self):
         return {'hello': 'world'}
