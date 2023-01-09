a0ec7300a6125f35892d6ef7929fd2cd3f2bf02f
PleYa <aaiaia.gg@gmail.com>
2023-01-09 12:27:55 +0900

    deleted white space

--- flask/basic/flask_serv.py
+++ flask/basic/flask_serv.py
@@ -5,22 +5,22 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return 'Hello World!\n'
- 
+
 @app.route('/home')
 def home():
     return 'Home Directory\n'
- 
+
 @app.route('/user/<user_name>/<int:user_int>')
 def user(user_name, user_int):
     return f'User Directory, user_name: {user_name}, user_int: {user_int}!!!\n'
- 
+
 @app.route('/set', methods = ['PUT'])
 def set():
     print('request: ' + str(request))
     data = request.json
     print('data: ' + str(data))
     return 'python get PUT method\n'
- 
+
 @app.route('/get', methods = ['GET'])
 def get():
     print('request: ' + str(request))
@@ -29,14 +29,14 @@ def get():
                 username='unknown'))
     contect_type='application/json'
     return data
- 
+
 @app.route('/post', methods = ['POST'])
 def post():
     print('request: ' + str(request))
     data = request.json
     print('data: ' + str(data))
     return 'python get POST method\n'
- 
+
 @app.route('/delete', methods = ['DELETE'])
 def delete():
     print('request: ' + str(request))
